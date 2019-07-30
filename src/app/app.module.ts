6e956ee1b0a6e67279bde1d31664558bb4f5446c
yvoncedric <yvoncedric@sfr.fr>
2019-07-30 17:14:03 +0200

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { Test1Component } from './test1/test1.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule,
+  RouterModule.forRoot([
+      { path: 'Test1', component: Test1Component },
+    ]) ],
+  declarations: [ AppComponent, HelloComponent, Test1Component ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
