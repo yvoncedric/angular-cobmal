d795a9309f7da24a25bc7322f1724936a248e4a9
yvoncedric <yvoncedric@sfr.fr>
2019-07-31 09:16:53 +0200

    2 files modified

--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test1',
-  template: '<h2> Boooooo !!!! ;)<h2>',
+  //template: '<h2> Boooooo !!!! ;)<h2>',
+  templateUrl: './test1.component.html',
   styleUrls: ['./test1.component.css']
 })
 export class Test1Component implements OnInit {
