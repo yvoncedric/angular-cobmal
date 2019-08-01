c921c99d8760f573a435104e92d922a337c326ad
yvoncedric <53308464+yvoncedric@users.noreply.github.com>
2019-08-01 17:31:34 +0200

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { Test1Component } from './test1/test1.component';
+import {NgbdDropdownBasicModule} from './dropdown-basic.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,
+  imports:      [ BrowserModule, FormsModule, NgbdDropdownBasicModule,
   RouterModule.forRoot([
       { path: 'Test1', component: Test1Component },
     ]) ],
